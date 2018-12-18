2aa225e9dccf12ad715f47f4296bd1844c46100e
Rene Ventura <venturar729@gmail.com>
2018-12-18 23:46:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  new_arr = array
+  if array.size == 1
+    return array[0]
+  end
+  new_arr[-1] = "and " + array[-1]
+  if array.size == 2
+    arr_to_str = new_arr.join(" ")
+  else
+    arr_to_str = new_arr.join(", ")
+  end
+  arr_to_str
 end
